ea1d8adff3ff2714a1a466600ecf38f7fa6b8bc5
divyajags <divyachowdaryjagarlamudi@gmail.com>
2023-07-25 16:50:40 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'; // Import the CSS file for styling
 
-function App() {
+const LoginPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    // Add your login logic here
+    // For demonstration purposes, we'll just log the email and password
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="login-container">
+      <h1 className="company-name">MYTimesheets</h1>
+      <p className="description">Please enter details to sign up</p>
+      <form onSubmit={handleLogin}>
+        <div className="input-container">
+          <label className="label">Email:</label>
+          <input
+            className="input-field"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="input-container">
+          <label className="label">Password:</label>
+          <input
+            className="input-field"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button className="login-button" type="submit">Sign Up</button>
+      </form>
+      <p className="login-text">Already have an account? <a href="/login">Login</a></p>
     </div>
   );
-}
+};
 
-export default App;
+export default LoginPage;
